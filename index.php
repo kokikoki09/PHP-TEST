d4e0df62967a1216690d94710a7946d9fa47634b
KOKI <coatchtech0613@gmail.com>
2021-10-30 16:24:29 +0900

    first-commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<table border="1" style="border-collapse: collapse">
+  <?php
+  for ($a = 1; $a <= 9; $a++) {
+    echo '<tr>';
+    for ($b = 1; $b <= 9; $b++) {
+      echo '<td>' . $a * $b . '</td>';
+    }
+    echo '</tr>';
+  }
+  ?>
+</table>
